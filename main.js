1e8b99016f352cf632e3fe39036acb1e53ec25da
Autumn Ragland <46455498+autumn-ragland@users.noreply.github.com>
2021-01-19 16:19:15 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,55 @@
+console.log(`HTML CSS JS To Do App`); // sanity check
+
+// select form fields
+let taskDescription = document.querySelector("#taskDescription");
+let taskUrgency = document.querySelector("#taskUrgency");
+let taskDue = document.querySelector("#taskDue");
+// form submit button
+let submitBtn = document.querySelector("button");
+// display tasks by urgency
+let urgentTasks = document.querySelector("#urgentTasks");
+let nonUrgentTasks = document.querySelector("#nonUrgentTasks");
+
+hideTask = (event) => {
+    // console.log(event.target); // check event target
+    let clickedTask = event.target;
+    let confirmDelete = confirm("Are you sure you want to complete this task?");
+    if(confirmDelete){
+        clickedTask.setAttribute("hidden", "");
+    }
+}
+
+
+submitForm = (event) => {
+    event.preventDefault(); // stop form from reloading
+
+    // // check field values
+    // console.log(taskDescription.value);
+    // console.log(taskUrgency.value);
+    // console.log(taskDue.value);
+
+    // add task to appropriate display element
+    if (taskUrgency.value == "urgent") {
+        urgentTasks.innerHTML += `<div class="card text-white bg-danger">Task : ${taskDescription.value} Date : ${taskDue.value}</div>`;
+
+        let urgentTaskItems = urgentTasks.querySelectorAll("div");
+        urgentTaskItems.forEach(task => {
+            task.addEventListener("click", hideTask);
+        });
+
+    } else {
+        nonUrgentTasks.innerHTML += `<div class="card text-white bg-secondary">Task : ${taskDescription.value} Date : ${taskDue.value}</div>`;
+
+        let nonUrgentTasksItems = nonUrgentTasks.querySelectorAll("div");
+        nonUrgentTasksItems.forEach(task => {
+            task.addEventListener("click", hideTask);
+        });
+    }
+
+    // clear form fields
+    taskDescription.value = "";
+    taskUrgency.value = "---";
+    taskDue.value = "";
+}
+
+submitBtn.addEventListener("click", submitForm);
